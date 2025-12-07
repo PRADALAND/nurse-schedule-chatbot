6e8df389a3008ab126524c16397b3f29e8f17690
PRADALAND <nursingla@snu.ac.kr>
2025-12-07 17:38:54 +0900

    Update file_store.py

--- utils/file_store.py
+++ utils/file_store.py
@@ -1,1 +1,17 @@
+import time
+from .supabase_client import get_supabase
+
+BUCKET = "chatbot-files"
+
+def upload_file(user_id: str, file_obj):
+    sb = get_supabase()
+    filename = f"{user_id}/{int(time.time())}_{file_obj.name}"
+    content = file_obj.read()
+
+    sb.storage.from_(BUCKET).upload(filename, content, {
+        "content-type": file_obj.type
+    })
+
+    url = sb.storage.from_(BUCKET).get_public_url(filename)
+    return filename, url
 
